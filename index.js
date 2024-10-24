4bf09c26bbc46e22832fd400c294d07d5e438584
Andrew <andrewlang.p@gmail.com>
2024-10-24 19:53:47 -0400

    add unit testing

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
-    console.log(desc);
+    console.log(desc[0].description);
   }
 });
